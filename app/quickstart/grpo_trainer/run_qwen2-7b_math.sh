42ff774071f2ff0487523195fa92736be0cc4383
qichang.dong <dongqichang@ecmas.ai>
2025-05-01 15:59:37 +0800

    add run_qwen2-7b_math.sh

---
+++ app/quickstart/grpo_trainer/run_qwen2-7b_math.sh
@@ -0,0 +1,54 @@
+set -x
+gpu=0,1,2,3,4,5,6,7
+export CUDA_VISIBLE_DEVICES=$gpu
+export VLLM_ATTENTION_BACKEND=XFORMERS
+export NUMEXPR_MAX_THREADS=64
+export WANDB_API_KEY=$(cat .wandb_api_key)
+wandb login
+gsm8k_train_path=/data/comm/openai/gsm8k/train.parquet
+gsm8k_test_path=/data/comm/openai/gsm8k/test.parquet
+math_train_path=/data/comm/DigitalLearningGmbH/MATH-lighteval/train.parquet
+math_test_path=/data/comm/DigitalLearningGmbH/MATH-lighteval/test.parquet
+
+train_files="['$gsm8k_train_path', '$math_train_path']"
+test_files="['$gsm8k_test_path', '$math_test_path']"
+
+python3 -m verl.trainer.main_ppo \
+    algorithm.adv_estimator=grpo \
+    data.train_files="$train_files" \
+    data.val_files="$test_files" \
+    data.train_batch_size=4 \
+    data.max_prompt_length=1024 \
+    data.max_response_length=1024 \
+    data.filter_overlong_prompts=True \
+    data.truncation='error' \
+    actor_rollout_ref.model.path=/data/model/Qwen/Qwen2.5-7B-Instruct \
+    actor_rollout_ref.actor.optim.lr=1e-6 \
+    actor_rollout_ref.model.use_remove_padding=True \
+    actor_rollout_ref.actor.ppo_mini_batch_size=2 \
+    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=8 \
+    actor_rollout_ref.actor.use_kl_loss=False \
+    actor_rollout_ref.actor.kl_loss_coef=0.001 \
+    actor_rollout_ref.actor.kl_loss_type=low_var_kl \
+    actor_rollout_ref.actor.entropy_coeff=0 \
+    actor_rollout_ref.model.enable_gradient_checkpointing=True \
+    actor_rollout_ref.actor.fsdp_config.param_offload=True \
+    actor_rollout_ref.actor.fsdp_config.optimizer_offload=True \
+    actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=8 \
+    actor_rollout_ref.rollout.tensor_model_parallel_size=2 \
+    actor_rollout_ref.rollout.name=vllm \
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.6 \
+    actor_rollout_ref.rollout.n=8 \
+    actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=8 \
+    actor_rollout_ref.ref.fsdp_config.param_offload=True \
+    algorithm.kl_ctrl.kl_coef=0.001 \
+    trainer.critic_warmup=0 \
+    trainer.logger=['console','wandb'] \
+    trainer.project_name='verl_grpo_example_gsm8k' \
+    trainer.experiment_name='qwen25_7b_function_rm' \
+    trainer.n_gpus_per_node=8 \
+    trainer.nnodes=1 \
+    trainer.save_freq=-1 \
+    trainer.test_freq=5 \
+    trainer.total_epochs=2 \
+    2>&1 | tee app/quickstart/grpo_trainer/grpo_demo.log
